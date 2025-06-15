e10e9d7c67112589bb32549c4b1190b0ba6d2be1
NinVidanov <ninovidanovic@hotmail.com>
2025-06-15 16:04:15 +0200

    initialize project

---
+++ rn-ispit/src/main/java/rs/ac/singidunum/rn_ispit/RnIspitApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.rn_ispit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnIspitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnIspitApplication.class, args);
+	}
+
+}
---
+++ rn-ispit/src/test/java/rs/ac/singidunum/rn_ispit/RnIspitApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.rn_ispit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnIspitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
